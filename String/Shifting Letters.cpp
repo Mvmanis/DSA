dea1bae7f3710c03ebe81d053f370c0c96a3de51
manish.vishwakarma <manish.vishwakarma@wisetechglobal.com>
2025-01-05 17:25:18 +0530

    Added POTD 5 Jan - I solution

---
+++ String/Shifting Letters.cpp
@@ -0,0 +1,57 @@
+    // Leetcode Link               : https://leetcode.com/problems/shifting-letters/description/
+
+
+
+/**************************************************************** C++ ****************************************************************/
+
+
+// Approach - 1  (Brute Force)
+
+//T.C : O(N*N)
+//S.C : O(1)
+
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        // brute force....
+        int n = shifts.size();
+        int j = 0;
+        int shiftIndex = 0;
+        while(shiftIndex<n){
+            int increment = shifts[shiftIndex];
+            for(int i = 0;i<=j;i++){
+                int val = s[i] - 'a';
+                val += increment;
+                val = val%26;
+                s[i] = 'a' + val;
+            }
+            j++;
+            shiftIndex++;
+        }
+        return s;
+    }
+};
+
+
+/********************************************************************************************************************************/
+
+
+//Approach-2 (Optimised)
+//T.C : O(N)
+//S.C : O(1)
+
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n = shifts.size();
+        long long totalShift = 0;
+        for(auto x : shifts) totalShift += x;
+        
+        for(int index = 0;index<s.size();index++){
+            s[index] = 'a' + (s[index] - 'a' + totalShift) % 26;
+            totalShift -= shifts[index]; 
+        }
+
+        return s;
+    }
+};
